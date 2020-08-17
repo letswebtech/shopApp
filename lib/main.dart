944690483c136646caf661bc05ef497967c53da5
Kashif Ahmed <letswebtech@gmail.com>
2020-08-18 00:05:33 +0530

    'theme and styling'

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My Shop",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+      ),
       home: ProductsOverviewScreen(),     
     );
   }
